acdb0dfff6e0e068a44367ba9e953d9d6e07cb2a
yuvinanuan <71350009+yuvinanuan@users.noreply.github.com>
2020-11-24 21:21:13 -0500

    Add files via upload

---
+++ JobCreator2.java
@@ -0,0 +1,61 @@
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+//jobCreator creates job, connects to jobSeeker, gives job, waits for job to finish and come back.
+//create job with type, computational task    i.e multiply, 2, 5. addition,subtraction,division,multiplication
+public class JobCreator{ //client
+    public static void main(String[] args) throws IOException{
+        String jobType;
+        String ip;
+        int var1;
+        int var2;
+        boolean isJobDone = false;
+        try{
+        Socket s = new Socket("localhost", 4999);
+
+        PrintWriter pr = new PrintWriter(s.getOutputStream());
+        Scanner sc = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+        //get job from user. one of the 4 types
+        System.out.println("Enter job type: addition, subtraction, division, or multiplication");
+        jobType = sc.nextLine();
+        pr.println(jobType);
+        pr.flush();
+        //get two ints from users
+        System.out.println("Enter 2 numbers");
+        var1 = sc.nextInt();
+        var2 = sc.nextInt();
+        pr.println(var1);
+        pr.flush();
+        pr.println(var2);
+        pr.flush();
+        //get ip address from user
+        System.out.println("Enter a IP Address");
+        ip = sc2.nextLine();
+        pr.println(ip);
+        pr.flush();
+        //get response
+        InputStreamReader in = new InputStreamReader(s.getInputStream());
+        BufferedReader bf = new BufferedReader(in);
+
+        String str = bf.readLine();
+        System.out.println("JobSeeker: "+ str+"\n");
+
+        str = bf.readLine();
+        System.out.println("JobSeeker: "+ str);
+
+        str = bf.readLine();
+        System.out.println(str);
+
+        int jobDoneNum = 0;
+        jobDoneNum = bf.read();
+        if (jobDoneNum == 1)
+            isJobDone = true;
+
+        
+
+        }catch(IOException e){
+            System.out.println("Connection failed. Abort");
+        }
+    }
+}
---
+++ JobSeeker2.java
@@ -0,0 +1,87 @@
+import java.net.*;
+import java.io.*;
+//job seeker waits for jobSeeker to connect, accepts job request, does job, reports back
+public class JobSeeker{ //server
+    public static void main(String[] args) throws UnknownHostException, IOException{
+        String jobString;
+        String jobIp;
+        int jobVar1;
+        int jobVar2;
+        boolean badJob = false;
+        boolean jobDone = false;
+        double sum = 0;
+        ServerSocket ss = new ServerSocket(4999);
+        Socket s = new Socket();
+            try {
+                s = ss.accept();
+                //server can only connect to one client at a time
+                System.out.println("JobCreator connected. Waiting for Job...");
+                InputStreamReader in = new InputStreamReader(s.getInputStream());
+                BufferedReader bf = new BufferedReader(in);
+
+                //get the job type from the creator
+                jobString = bf.readLine();
+                System.out.println("jobCreator: " + jobString);
+
+                //get the 2 variables from creator
+                jobVar1 = Integer.parseInt(bf.readLine());
+                jobVar2 = Integer.parseInt(bf.readLine());
+                System.out.println("var1: " + jobVar1);
+                System.out.println("var2: " + jobVar2);
+
+                //get ip from creator
+                jobIp = bf.readLine();
+                System.out.println("IP: " + jobIp);
+
+
+                PrintWriter pr = new PrintWriter(s.getOutputStream());
+                pr.println("RECEIVED. Job Type: " + jobString + ", var1: " + jobVar1 + ", var2: " + jobVar2);
+                pr.flush();
+                //do the calculations
+                switch (jobString) {
+                    case "addition": sum = jobVar1 + jobVar2;
+                        break;
+                    case "subtraction": sum = jobVar1 - jobVar2;
+                        break;
+                    case "division": sum = (double) (jobVar1 / jobVar2);
+                        break;
+                    case "multiplication": sum = jobVar1 *  jobVar2;
+                        break;
+                }
+                if (badJob == false) {
+                    System.out.println("Job finished.");
+                    pr.println("Job Complete. sum = " + sum);
+                    pr.flush();
+
+
+                    //Check if IP is online or not
+                    InetAddress addr = InetAddress.getByName(jobIp);
+                    if(addr.isReachable(5000)) {
+                        System.out.println("IP online");
+                        pr.println("This IP Address is online");
+                    } else {
+                        System.out.println("IP offline");
+                        pr.println("This IP Address is offline");
+                    }                   
+
+                    jobDone = true;
+                    pr.println(1);
+                    pr.flush();
+
+                } else {
+                    System.out.println("Job incomplete. Error.");
+                    pr.println("Error. Unknown job type.");
+                    pr.flush();
+                    pr.println(0);
+                    pr.flush();
+                }
+
+
+
+                //end socket for this job.
+                s.close();
+            }catch(IOException ioe){
+                System.out.println("Error. JobCreator disconnected.");
+            }
+    }
+}
