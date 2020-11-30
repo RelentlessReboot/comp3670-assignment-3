7792412a51941fddcedefc92c17e1c3b55a8a323
Rel <72431760+RelentlessReboot@users.noreply.github.com>
2020-11-29 22:10:12 -0500

    Further clean up

--- JobCreator.java
+++ JobCreator.java
@@ -1,43 +1,60 @@
 import java.net.*;
 import java.io.*;
 import java.util.Scanner;
-//jobCreator creates job, connects to jobSeeker, gives job, waits for job to finish and come back.
-//create job with type, computational task    i.e multiply, 2, 5. addition,subtraction,division,multiplication
-public class JobCreator{ //client
+//jobCreator creates job, connects to JobSeeker, assigns job, and receives the executed job report from JobSeeker.
+//create job with type 1 (A3P2Q1 Job 1), 2 (A3P2Q1 Job 2), 3 (A3P2Q2 Job 2), or 4 (A3P2Q2 Job 3)
+public class JobCreator2{ //client
     public static void main(String[] args) throws IOException{
         String jobType;
-        int var1;
-        int var2;
+        String ip;
+        String port;
         boolean isJobDone = false;
         try{
         Socket s = new Socket("localhost", 4999);
 
         PrintWriter pr = new PrintWriter(s.getOutputStream());
         Scanner sc = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+        Scanner sc3 = new Scanner(System.in);
         //get job from user. one of the 4 types
-        System.out.println("Enter job type: addition, subtraction, division, or multiplication");
+        System.out.println("Enter job type:");
+        //job 1 = A3 Q1 Job 1
+        System.out.println("1 = Detect if an IP address/host name is online");
+        //job 2 = A3 Q1 Job 2
+        System.out.println("2 = Detect the status of a port at an IP address");
+        //job 3 = A3 Q2 Job 2
+        System.out.println("3 = Execute a TCP flood attack against a port at an IP address");
+        //job 4 = A3 Q2 Job 3
+        System.out.println("4 = Execute a UDP flood attack against a port at an IP address");
         jobType = sc.nextLine();
         pr.println(jobType);
         pr.flush();
-        //get two ints from users
-        System.out.println("Enter 2 numbers");
-        var1 = sc.nextInt();
-        var2 = sc.nextInt();
-        pr.println(var1);
+        //get ip address from user
+        System.out.println("Enter an IP Address/host name:");
+        ip = sc2.nextLine();
+        pr.println(ip);
         pr.flush();
-        pr.println(var2);
-        pr.flush();
+        //If job type is not Job 1, get port number from user
+        if (!jobType.equalsIgnoreCase("1")){
+            System.out.println("Enter a port number:");
+            port = sc3.nextLine();
+            pr.println(port);
+            pr.flush();
+        }
         //get response
         InputStreamReader in = new InputStreamReader(s.getInputStream());
         BufferedReader bf = new BufferedReader(in);
 
         String str = bf.readLine();
-        System.out.println("JobSeeker: "+ str+"\n");
+        System.out.println("JobSeeker: "+ str +"\n");
 
         str = bf.readLine();
         System.out.println("JobSeeker: "+ str);
-        int jobDoneNum = 0;
-        jobDoneNum = bf.read();
+
+        str = bf.readLine();
+        System.out.println(str);
+
+        int jobDoneNum = bf.read();
         if (jobDoneNum == 1)
             isJobDone = true;
         }catch(IOException e){
--- JobSeeker.java
+++ JobSeeker.java
@@ -1,16 +1,15 @@
 import java.net.*;
 import java.io.*;
 //job seeker waits for jobSeeker to connect, accepts job request, does job, reports back
-public class JobSeeker{ //server
+public class JobSeeker2{ //server
     public static void main(String[] args) throws IOException{
         String jobString;
-        int jobVar1;
-        int jobVar2;
+        String jobIp;
+        String jobPort;
         boolean badJob = false;
         boolean jobDone = false;
-        double sum = 0;
         ServerSocket ss = new ServerSocket(4999);
-        Socket s = new Socket();
+        Socket s;
             try {
                 s = ss.accept();
                 //server can only connect to one client at a time
@@ -21,43 +20,152 @@ public class JobSeeker{ //server
                 //get the job type from the creator
                 jobString = bf.readLine();
                 System.out.println("jobCreator: " + jobString);
+                //get IP address from JobCreator
+                jobIp = bf.readLine();
+                System.out.println("IP: " + jobIp);
 
-                //get the 2 variables from creator
-                jobVar1 = Integer.parseInt(bf.readLine());
-                jobVar2 = Integer.parseInt(bf.readLine());
-                System.out.println("var1: " + jobVar1);
-                System.out.println("var2: " + jobVar2);
+                //get port number from JobCreator
+                jobPort = bf.readLine();
+                System.out.println("Port: " + jobPort);
 
-                PrintWriter pr = new PrintWriter(s.getOutputStream());
-                pr.println("RECEIVED. Job Type: " + jobString + ", var1: " + jobVar1 + ", var2: " + jobVar2);
-                pr.flush();
-                //do the calculations
-                switch (jobString) {
-                    case "addition", "Addition" -> sum = jobVar1 + jobVar2;
-                    case "subtraction", "Subtraction" -> sum = jobVar1 - jobVar2;
-                    case "division", "Division" -> sum = (double) (jobVar1 / jobVar2);
-                    case "multiplication", "Multiplication" -> sum = jobVar1 * jobVar2;
-                    default -> badJob = true;
+                try (PrintWriter pr = new PrintWriter(s.getOutputStream())) {
+                    pr.println("RECEIVED. Job Type: " + jobString + "/n" + "IP: " + jobIp + ", Port: " + jobPort);
+                    pr.flush();
+                    //determine if job type is valid
+                    switch (jobString) {
+                        case "1":
+                        case "2":
+                        case "3":
+                        case "4":
+                            break;
+                        default:
+                            badJob = true;
+                            pr.println("Error. Invalid job type.");
+                            pr.flush();
+                            break;
+                    }
+                    if (badJob) {
+                        System.out.println("Job incomplete. Error.");
+                        pr.println("Error. Unknown job type.");
+                        pr.flush();
+                        pr.println(0);
+                        pr.flush();
+                    } else {
+                        //do the job based on job type
+                        //assign part of create-assign-execute-report process for each job
+                        switch (jobString) {
+                            case "1":
+                                //assign 1st job to JobSeeker, Q1 Job 1
+                                if (isOnline(jobIp)) {
+                                    System.out.println("IP online");
+                                    pr.println("This IP Address/hostname is online.");
+                                    pr.flush();
+                                } else {
+                                    System.out.println("IP offline");
+                                    pr.println("This IP Address/hostname is offline.");
+                                    pr.flush();
+                                }
+                                break;
+                            case "2":
+                                //assign 2nd job to JobSeeker, Q1 Job 2
+                                //detect status of port jobPort at IP address jobIP
+                                String pstat = portStatus(jobIp, jobPort);
+                                //report part of create-assign-execute-report process for A3P2Q1 Job 2
+                                if(pstat.equalsIgnoreCase("open")){
+                                    System.out.println("Port Open");
+                                    pr.println("Port " + jobPort + " at IP address " + jobIp + " is open.");
+                                    pr.flush();
+                                } else if (pstat.equalsIgnoreCase("closed")){
+                                    System.out.println("Port Closed");
+                                    pr.println("Port " + jobPort + " at IP address " + jobIp + " is closed.");
+                                    pr.flush();
+                                } else if(pstat.equalsIgnoreCase("filtered")){
+                                    System.out.println("Port Filtered");
+                                    pr.println("Port " + jobPort + " at IP address " + jobIp + " is filtered.");
+                                    pr.flush();
+                                } else {
+                                    System.out.println("Status Unknown");
+                                    pr.println("The status of port " + jobPort + " at IP address " + jobIp + " is unknown.");
+                                    pr.flush();
+                                }
+                                break;
+                            case "3":
+                                //assign 3rd job to JobSeeker, Q2 Job 2
+                                thirdJob(jobIp, jobPort);
+                                break;
+                            case "4":
+                                //assign 4th job to JobSeeker, Q2 Job 3
+                                fourthJob(jobIp, jobPort);
+                                break;
+                            default:
+                                pr.println("Error. Job not assigned.");
+                                pr.flush();
+                                break;
+                        }
+
+                        System.out.println("Job finished.");
+                        pr.println("Job Complete.");
+                        pr.flush();
+                        jobDone = true;
+                        pr.println(1);
+                        pr.flush();
+                    }
                 }
-                if (!badJob) {
-                    System.out.println("Job finished.");
-                    pr.println("Job Complete. sum = " + sum);
-                    pr.flush();
-                    jobDone = true;
-                    pr.println(1);
-                    pr.flush();
-                } else {
-                    System.out.println("Job incomplete. Error.");
-                    pr.println("Error. Unknown job type.");
-                    pr.flush();
-                    pr.println(0);
-                    pr.flush();
-                }
-
                 //end socket for this job.
                 s.close();
-            }catch(IOException ioe){
+            } catch(IOException ioe){
                 System.out.println("Error. JobCreator disconnected.");
             }
+    }
+    //job 1 = check if given IP address is online or not
+    //execute part of create-assign-execute-report process for A3P2Q1 Job 1
+    public static boolean isOnline(String jobIp) throws IOException {
+        return InetAddress.getByName(jobIp).isReachable(5000);
+    }
+    //job 2 = check status of given port number at given IP address
+    //execute part of create-assign-execute-report process for A3P2Q1 Job 2
+    public static String portStatus(String jobIp, String jobPort) {
+        int port = Integer.parseInt(jobPort);
+        Socket sckt = null;//create socket variable
+        String status = "unknown";
+        try {
+            //connection to port at host established, new socket
+            sckt = new Socket(jobIp, port);
+        } catch (IOException e) {
+            if(e.getMessage().equals("Connection refused")){
+                //returns port status as closed
+                status = "closed";
+            }
+            if(e instanceof SocketTimeoutException){
+                //connection timed out, port blocked by firewall
+                //returns port status as filtered
+                status = "filtered";
+            }
+        } finally {
+            //checks if port is not filtered
+            if (sckt != null) {
+                //returns port status as open
+                if (sckt.isConnected()){
+                    status = "open";
+                }
+                try {
+                    //closes connection
+                    sckt.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return status;
+    }
+    //job 3 = code to do third job
+    //execute part of create-assign-execute-report process for A3P2Q2 Job 2
+    public static void thirdJob(String jobIp, String jobPort) {
+        
+    }
+    //job 4 = code to do fourth job
+    //execute part of create-assign-execute-report process for A3P2Q2 Job 3
+    public static void fourthJob(String jobIp, String jobPort) {
+        
     }
 }
